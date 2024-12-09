aa4569994fff934d2f18002f2449af397d17170a
chendler <antwerpen00@gmail.com>
2024-12-10 00:03:39 +0700

    Update auto-pr.ps1
    Replace the placeholder repository

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "chendler/MyScoopBucket:main"
+    [String]$upstream = "chendler/MyScoopBucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
